361891af2468e46f67e8c4908a056fbd6c8d3816
Prakhar Shukla <prakhar230125@gmail.com>
2026-02-03 17:22:42 +0530

    Proper Structure and add login in app

--- basic-app/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "<h1>Welcome to your Basic App</h1>";
-echo "<p>This is a simple PHP application setup in XAMPP.</p>";
-phpinfo();
-?>
--- cloud_kitchen_project-dev/README.md
+++
@@ -1,2 +0,0 @@
-# cloud_kitchen_project
-This is cloud kitchen project
